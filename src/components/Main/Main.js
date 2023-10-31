1a06d72a16867843f5d0510d11ca230724d73b05
Trevor Southworth <tsouthworth8@gmail.com>
2023-10-31 14:41:59 -0400

    Test server

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import "./Main.css";
+import axios from "axios";
 
-function Main(size) {
-    const currentSize = size.size;
+function Main(screenSize) {
+    // holds user devices creen size
+    const currentSize = screenSize.size;
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get('http://45.33.89.127/hi')
+        .then(resp => {
+            if(resp) {
+                setData(resp.data); 
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <Fragment>
             <div className='Main'>
@@ -12,6 +28,8 @@ function Main(size) {
                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                     hi, thanks for using your {currentSize}
+                    <br />
+                    here is data: {JSON.stringify(data)}
                 </div>
             </div>
         </Fragment>
